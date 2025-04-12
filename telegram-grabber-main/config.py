275974d965f1b00a8841d6327d6accec6fa49e80
Orbit-Cristiano <148749659+Orbit-Cristiano@users.noreply.github.com>
2025-04-12 18:46:56 +0700

    Add files via upload

---
+++ telegram-grabber-main/config.py
@@ -0,0 +1,12 @@
+api_id = 28027556
+api_hash = '3f4d9d1ffbb752e056089e56ad21be63'
+bot_token = '8059514172:AAGh2EGheUj2ivdxTI9zpUkhd6O7ZSLbgTQ'
+my_id = 2068368438
+
+technical_channel_id = 8059514172  # ID технического канала
+
+new_link = "http://t.me/"
+new_username = "@"
+
+proxy_url = 'http://ip:port'  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
+openai_api_key = "sk-..."  #заполнять только если планируете рерайтить текст в режиме модерации с помощью Chat GPT
